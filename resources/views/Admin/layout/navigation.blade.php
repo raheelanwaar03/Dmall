57bdbc55d4a8cd5bbb90810c022cbafa11b9707b
Raheel Anwaar <raheelanwaar0316@gmail.com>
2022-10-30 19:12:27 +0500

    admin can Logout now

--- resources/views/Admin/layout/navigation.blade.php
+++ resources/views/Admin/layout/navigation.blade.php
@@ -201,7 +201,7 @@
                                 </a>
 
                                 <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="{{ route('logout') }}" data-toggle="modal"
+                                <a class="dropdown-item" data-toggle="modal"
                                     data-target="#logoutModal">
                                     <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
                                     Logout
