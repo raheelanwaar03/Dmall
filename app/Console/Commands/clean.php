4ddd78969a25b29d691aa906e70b25b07f8c6843
Raheel Anwaar <raheelanwaar0316@gmail.com>
2022-11-03 16:48:53 +0500

    only new user will enter in database

--- app/Console/Commands/clean.php
+++ app/Console/Commands/clean.php
@@ -63,158 +63,158 @@ class clean extends Command
 
         // make new categories
 
-        $catagory = new Catagory();
-        $catagory->catagory_name = 'Shirt';
-        $catagory->catagory_des = 'All kind of Shirts with new design and sizes';
-        $catagory->catagory_img = '3796419.png';
-        $catagory->save();
+        // $catagory = new Catagory();
+        // $catagory->catagory_name = 'Shirt';
+        // $catagory->catagory_des = 'All kind of Shirts with new design and sizes';
+        // $catagory->catagory_img = '3796419.png';
+        // $catagory->save();
 
-        $catagory = new Catagory();
-        $catagory->catagory_name = 'Sports';
-        $catagory->catagory_des = 'All kind of Sports with new design and sizes';
-        $catagory->catagory_img = '5443825.png';
-        $catagory->save();
+        // $catagory = new Catagory();
+        // $catagory->catagory_name = 'Sports';
+        // $catagory->catagory_des = 'All kind of Sports with new design and sizes';
+        // $catagory->catagory_img = '5443825.png';
+        // $catagory->save();
 
-        $catagory = new Catagory();
-        $catagory->catagory_name = 'Fashion';
-        $catagory->catagory_des = 'All kind of Fashion with new design and sizes';
-        $catagory->catagory_img = '5071230.png';
-        $catagory->save();
+        // $catagory = new Catagory();
+        // $catagory->catagory_name = 'Fashion';
+        // $catagory->catagory_des = 'All kind of Fashion with new design and sizes';
+        // $catagory->catagory_img = '5071230.png';
+        // $catagory->save();
 
-        $catagory = new Catagory();
-        $catagory->catagory_name = 'Electronic';
-        $catagory->catagory_des = 'All kind of Electronic with new design and sizes';
-        $catagory->catagory_img = '7786715.png';
-        $catagory->save();
+        // $catagory = new Catagory();
+        // $catagory->catagory_name = 'Electronic';
+        // $catagory->catagory_des = 'All kind of Electronic with new design and sizes';
+        // $catagory->catagory_img = '7786715.png';
+        // $catagory->save();
 
-        $catagory = new Catagory();
-        $catagory->catagory_name = 'Furniture';
-        $catagory->catagory_des = 'All kind of Furniture with new design and sizes';
-        $catagory->catagory_img = '5871713.png';
-        $catagory->save();
+        // $catagory = new Catagory();
+        // $catagory->catagory_name = 'Furniture';
+        // $catagory->catagory_des = 'All kind of Furniture with new design and sizes';
+        // $catagory->catagory_img = '5871713.png';
+        // $catagory->save();
 
-        $catagory = new Catagory();
-        $catagory->catagory_name = 'Shoes';
-        $catagory->catagory_des = 'All kind of Shoes with new design and sizes';
-        $catagory->catagory_img = '6351976.png';
-        $catagory->save();
-
-
-        // Adding new Products
-
-        $product = new ProductManger();
-        $product->product_name = 'Red Shirt';
-        $product->product_id = '#122222';
-        $product->product_catagory = 'Shirt';
-        $product->product_description = 'Red Shirt with all sizes and designs';
-        $product->product_price = '500';
-        $product->product_qty = '5';
-        $product->product_img = '6884505.png';
-        $product->save();
-
-        $product = new ProductManger();
-        $product->product_name = 'Product';
-        $product->product_id = '#2';
-        $product->product_catagory = 'Shoes';
-        $product->product_description = 'Product with all sizes and designs';
-        $product->product_price = '900';
-        $product->product_qty = '5';
-        $product->product_img = '558909.png';
-        $product->save();
-
-        $product = new ProductManger();
-        $product->product_name = 'Product';
-        $product->product_id = '#3212';
-        $product->product_catagory = 'Fashion';
-        $product->product_description = 'Product with all sizes and designs';
-        $product->product_price = '4300';
-        $product->product_qty = '5';
-        $product->product_img = '7395015.png';
-        $product->save();
-
-        $product = new ProductManger();
-        $product->product_name = 'Product';
-        $product->product_id = '#7654';
-        $product->product_discount = 50;
-        $product->product_catagory = 'Sports';
-        $product->product_description = 'Product Description with all sizes and designs';
-        $product->product_price = '500';
-        $product->product_qty = '5';
-        $product->product_img = '3796419.png';
-        $product->save();
-
-        $product = new ProductManger();
-        $product->product_name = 'Product';
-        $product->product_id = '#984';
-        $product->product_catagory = 'Fashion';
-        $product->product_description = 'Product Description with all sizes and designs';
-        $product->product_price = '500';
-        $product->product_qty = '5';
-        $product->product_img = '5071230.png';
-        $product->save();
-
-        $product = new ProductManger();
-        $product->product_name = 'Product';
-        $product->product_id = '#1222';
-        $product->product_catagory = 'Shirt';
-        $product->product_description = 'Product Description with all sizes and designs';
-        $product->product_price = '500';
-        $product->product_qty = '5';
-        $product->product_img = '726311.png';
-        $product->save();
-
-        $product = new ProductManger();
-        $product->product_name = 'Product';
-        $product->product_id = '#524';
-        $product->product_catagory = 'Electronic';
-        $product->product_description = 'Product with all sizes and designs';
-        $product->product_price = '7000';
-        $product->product_qty = '6';
-        $product->product_img = '297342.png';
-        $product->save();
-
-        $product = new ProductManger();
-        $product->product_name = 'Belt';
-        $product->product_id = '#097';
-        $product->product_catagory = 'Furniture';
-        $product->product_description = 'Product Description with all sizes and designs';
-        $product->product_price = '300';
-        $product->product_qty = '9';
-        $product->product_img = '2452533.png';
-        $product->save();
+        // $catagory = new Catagory();
+        // $catagory->catagory_name = 'Shoes';
+        // $catagory->catagory_des = 'All kind of Shoes with new design and sizes';
+        // $catagory->catagory_img = '6351976.png';
+        // $catagory->save();
 
 
-        $product = new ProductManger();
-        $product->product_name = 'Fashion';
-        $product->product_id = '#124422';
-        $product->product_catagory = 'Fashion';
-        $product->product_description = 'Product Description with all sizes and designs';
-        $product->product_price = '100';
-        $product->product_qty = '3';
-        $product->product_img = '619851.png';
-        $product->save();
+        // // Adding new Products
 
-        $product = new ProductManger();
-        $product->product_name = 'Product';
-        $product->product_id = '#122222';
-        $product->product_catagory = 'Sports';
-        $product->product_description = 'Product Description with all sizes and designs';
-        $product->product_price = '50055';
-        $product->product_qty = '1';
-        $product->product_img = '558909.png';
-        $product->save();
+        // $product = new ProductManger();
+        // $product->product_name = 'Red Shirt';
+        // $product->product_id = '#122222';
+        // $product->product_catagory = 'Shirt';
+        // $product->product_description = 'Red Shirt with all sizes and designs';
+        // $product->product_price = '500';
+        // $product->product_qty = '5';
+        // $product->product_img = '6884505.png';
+        // $product->save();
+
+        // $product = new ProductManger();
+        // $product->product_name = 'Product';
+        // $product->product_id = '#2';
+        // $product->product_catagory = 'Shoes';
+        // $product->product_description = 'Product with all sizes and designs';
+        // $product->product_price = '900';
+        // $product->product_qty = '5';
+        // $product->product_img = '558909.png';
+        // $product->save();
+
+        // $product = new ProductManger();
+        // $product->product_name = 'Product';
+        // $product->product_id = '#3212';
+        // $product->product_catagory = 'Fashion';
+        // $product->product_description = 'Product with all sizes and designs';
+        // $product->product_price = '4300';
+        // $product->product_qty = '5';
+        // $product->product_img = '7395015.png';
+        // $product->save();
+
+        // $product = new ProductManger();
+        // $product->product_name = 'Product';
+        // $product->product_id = '#7654';
+        // $product->product_discount = 50;
+        // $product->product_catagory = 'Sports';
+        // $product->product_description = 'Product Description with all sizes and designs';
+        // $product->product_price = '500';
+        // $product->product_qty = '5';
+        // $product->product_img = '3796419.png';
+        // $product->save();
+
+        // $product = new ProductManger();
+        // $product->product_name = 'Product';
+        // $product->product_id = '#984';
+        // $product->product_catagory = 'Fashion';
+        // $product->product_description = 'Product Description with all sizes and designs';
+        // $product->product_price = '500';
+        // $product->product_qty = '5';
+        // $product->product_img = '5071230.png';
+        // $product->save();
+
+        // $product = new ProductManger();
+        // $product->product_name = 'Product';
+        // $product->product_id = '#1222';
+        // $product->product_catagory = 'Shirt';
+        // $product->product_description = 'Product Description with all sizes and designs';
+        // $product->product_price = '500';
+        // $product->product_qty = '5';
+        // $product->product_img = '726311.png';
+        // $product->save();
+
+        // $product = new ProductManger();
+        // $product->product_name = 'Product';
+        // $product->product_id = '#524';
+        // $product->product_catagory = 'Electronic';
+        // $product->product_description = 'Product with all sizes and designs';
+        // $product->product_price = '7000';
+        // $product->product_qty = '6';
+        // $product->product_img = '297342.png';
+        // $product->save();
+
+        // $product = new ProductManger();
+        // $product->product_name = 'Belt';
+        // $product->product_id = '#097';
+        // $product->product_catagory = 'Furniture';
+        // $product->product_description = 'Product Description with all sizes and designs';
+        // $product->product_price = '300';
+        // $product->product_qty = '9';
+        // $product->product_img = '2452533.png';
+        // $product->save();
 
 
-        $product = new ProductManger();
-        $product->product_name = 'Mobile';
-        $product->product_id = '#37338';
-        $product->product_discount = 30;
-        $product->product_catagory = 'Electronic';
-        $product->product_description = 'Mobile with all sizes and designs';
-        $product->product_price = '50000';
-        $product->product_qty = '3';
-        $product->product_img = '284793.png';
-        $product->save();
+        // $product = new ProductManger();
+        // $product->product_name = 'Fashion';
+        // $product->product_id = '#124422';
+        // $product->product_catagory = 'Fashion';
+        // $product->product_description = 'Product Description with all sizes and designs';
+        // $product->product_price = '100';
+        // $product->product_qty = '3';
+        // $product->product_img = '619851.png';
+        // $product->save();
+
+        // $product = new ProductManger();
+        // $product->product_name = 'Product';
+        // $product->product_id = '#122222';
+        // $product->product_catagory = 'Sports';
+        // $product->product_description = 'Product Description with all sizes and designs';
+        // $product->product_price = '50055';
+        // $product->product_qty = '1';
+        // $product->product_img = '558909.png';
+        // $product->save();
+
+
+        // $product = new ProductManger();
+        // $product->product_name = 'Mobile';
+        // $product->product_id = '#37338';
+        // $product->product_discount = 30;
+        // $product->product_catagory = 'Electronic';
+        // $product->product_description = 'Mobile with all sizes and designs';
+        // $product->product_price = '50000';
+        // $product->product_qty = '3';
+        // $product->product_img = '284793.png';
+        // $product->save();
 
         return 'SUCCESS';
     }
